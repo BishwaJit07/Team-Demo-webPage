9c42ccda62ba2b5ce97c8885a11e2588cbc3e398
KhadizaSamiha <khadizasamiha7@gmail.com>
2023-08-03 17:46:15 +0600

    footer updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,9 @@ const Footer = () => {
         <footer className="fixed bottom-0 border-t-2 rounded-lg border-sky-400 bg-gradient-to-r from-blue-900 via-blue-700 to-pink-300  footer p-10 text-base-100">
             <div className="font-medium text-sm">
                 <span className="footer-title">Team Heros__</span>
-                <Link to='' className="link link-hover">Biswajit</Link>
-                <Link to='https://jobayarportfolio.netlify.app/' className="link link-hover">Prodip</Link>
+                <Link to='https://dev-prodip-kumar-shil.web.app/' className="link link-hover">Prodip</Link>
+                <Link to='https://portfolio-5c3g.vercel.app/' className="link link-hover">Naime</Link>
+                <Link to='https://jobayarportfolio.netlify.app/' className="link link-hover">Jobayer</Link>
                 <Link to='https://hasiburhossainsanto.netlify.app/' className="link link-hover">Santo</Link>
                 <Link to='https://sensational-tapioca-c29036.netlify.app' className="link link-hover">Samiha</Link>
             </div>
